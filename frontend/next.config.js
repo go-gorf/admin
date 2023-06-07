7267d796737aa5afac71383072c8657022e5e50d
Tom Victor <vjtomvictor@icloud.com>
2023-06-07 16:47:12 +0200

    remove frontend

--- frontend/next.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: 'export',
-    basePath: '/admin',
-    assetPrefix: '/admin/',
-}
-
-module.exports = nextConfig
--- frontend/out/_next/static/chunks/app/page-516bf5812c1ca914.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[931],{2997:function(n,e,u){Promise.resolve().then(u.t.bind(u,7533,23))},7533:function(){}},function(n){n.O(0,[667,488,744],function(){return n(n.s=2997)}),_N_E=n.O()}]);
